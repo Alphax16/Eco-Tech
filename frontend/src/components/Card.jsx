30dd530708da54d9dafe6bb8e7aceb309477597a
anand4756 <anand.k4756@gmail.com>
2023-09-27 23:17:22 +0530

    changes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -20,16 +20,28 @@ const Card = ({ title, imageSrc, description, linkTo }) => {
         initial={{ x: "-70vw" }}
         animate={{ x: 0 }}
         transition={{ duration: 1, origin: 1, delay: 0.3 }}
+        
         >
         <Link to={linkTo}>
             <Box
             maxW={'345px'}
             w={'full'}
             bg={useColorModeValue('white', 'gray.900')}
-            boxShadow={'2xl'}
+            
             rounded={'md'}
             p={6}
             overflow={'hidden'}
+            borderRadius="md"
+            boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
+
+            transition="box-shadow 0.3s, transform 0.3s"
+            _hover={{
+                boxShadow: "0px 8px 12px rgba(1, 0, 0, 0.2)", 
+
+            
+              transform: "scale(1.05)", 
+            }}
+      
             >
             <Box h={'210px'} overflow={'hidden'} bg={'gray.100'} mt={-6} mx={-6} mb={6} display={'block'}>
                 <Image src={imageSrc} width={'345px'} height={'230px'} alt="Example" />
